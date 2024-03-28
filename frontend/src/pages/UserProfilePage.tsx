906bdceab721c9cf7bc594ed9ed4b5c33de1ded4
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-03-28 13:00:00 +0600

    getUserInfo

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,24 @@
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
-import { useUpdateMyUser } from "@/api/MyUserApi";
+import { useUpdateMyUser, useGetMyUser } from "@/api/MyUserApi";
 
 const UserProfilePage = () => {
-  const { updateUser, isLoading } = useUpdateMyUser();
+  const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+  const { updateUser, isLoading: isUpdateloading } = useUpdateMyUser();
 
-  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
+  if (isGetLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateloading}
+    />
+  );
 };
 
 export default UserProfilePage;
