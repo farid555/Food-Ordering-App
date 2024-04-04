df9d3708bf31144033784a06f2f839af68041b7a
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-04-04 10:50:56 +0600

    search API request

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,8 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 
 const SearchPage = () => {
   const { city } = useParams();
-  return <span>SearchPage {city}</span>;
+  const { results } = useSearchRestaurants(city);
+
+  return (
+    <span>
+      User searched for {city}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 };
 
 export default SearchPage;
