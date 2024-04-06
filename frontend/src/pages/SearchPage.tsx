8c99fb97e8f851f1f16355ee201bd4d5b6f923cb
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-04-06 09:21:23 +0600

    search page layout

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,21 +1,30 @@
 import { useParams } from "react-router-dom";
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchResultCard from "@/components/SearchResultCard";
 
 const SearchPage = () => {
   const { city } = useParams();
-  const { results } = useSearchRestaurants(city);
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  if (!isLoading) {
+    <span>Page Loading</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
 
   return (
-    <span>
-      User searched for {city}
-      <span>
-        {results?.data.map((restaurant) => (
-          <span>
-            found - {restaurant.restaurantName}, {restaurant.city}
-          </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">insert cuisines here :)</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
-      </span>
-    </span>
+      </div>
+    </div>
   );
 };
 
